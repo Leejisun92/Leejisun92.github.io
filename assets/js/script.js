1f6e041dafb82b5e9879c439b4687f9304c208af
cym-2 <24cocoa@naver.com>
2022-11-09 11:20:58 +0900

    chage category name

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 // element toggle function
 const elementToggleFunc = function (elem) {
-    elem.classList.toggle('active');
+   elem.classList.toggle('active');
 };
 
 // sidebar variables
@@ -11,7 +11,7 @@ const sidebarBtn = document.querySelector('[data-sidebar-btn]');
 
 // sidebar toggle functionality for mobile
 sidebarBtn.addEventListener('click', function () {
-    elementToggleFunc(sidebar);
+   elementToggleFunc(sidebar);
 });
 
 // custom select variables
@@ -21,47 +21,47 @@ const selectValue = document.querySelector('[data-selecct-value]');
 const filterBtn = document.querySelectorAll('[data-filter-btn]');
 
 select.addEventListener('click', function () {
-    elementToggleFunc(this);
+   elementToggleFunc(this);
 });
 
 // add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
-    selectItems[i].addEventListener('click', function () {
-        let selectedValue = this.innerText.toLowerCase();
-        selectValue.innerText = this.innerText;
-        elementToggleFunc(select);
-        filterFunc(selectedValue);
-    });
+   selectItems[i].addEventListener('click', function () {
+      let selectedValue = this.innerText.toLowerCase();
+      selectValue.innerText = this.innerText;
+      elementToggleFunc(select);
+      filterFunc(selectedValue);
+   });
 }
 
 // filter variables
 const filterItems = document.querySelectorAll('[data-filter-item]');
 
 const filterFunc = function (selectedValue) {
-    for (let i = 0; i < filterItems.length; i++) {
-        if (selectedValue === 'all') {
-            filterItems[i].classList.add('active');
-        } else if (selectedValue === filterItems[i].dataset.category) {
-            filterItems[i].classList.add('active');
-        } else {
-            filterItems[i].classList.remove('active');
-        }
-    }
+   for (let i = 0; i < filterItems.length; i++) {
+      if (selectedValue === 'all') {
+         filterItems[i].classList.add('active');
+      } else if (selectedValue === filterItems[i].dataset.category.toLowerCase()) {
+         filterItems[i].classList.add('active');
+      } else {
+         filterItems[i].classList.remove('active');
+      }
+   }
 };
 
 // add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
-    filterBtn[i].addEventListener('click', function () {
-        let selectedValue = this.innerText.toLowerCase();
-        selectValue.innerText = this.innerText;
-        filterFunc(selectedValue);
+   filterBtn[i].addEventListener('click', function () {
+      let selectedValue = this.innerText.toLowerCase();
+      selectValue.innerText = this.innerText;
+      filterFunc(selectedValue);
 
-        lastClickedBtn.classList.remove('active');
-        this.classList.add('active');
-        lastClickedBtn = this;
-    });
+      lastClickedBtn.classList.remove('active');
+      this.classList.add('active');
+      lastClickedBtn = this;
+   });
 }
 
 // contact form variables
@@ -71,14 +71,14 @@ const formBtn = document.querySelector('[data-form-btn]');
 
 // add event to all form input field
 for (let i = 0; i < formInputs.length; i++) {
-    formInputs[i].addEventListener('input', function () {
-        // check form validation
-        if (form.checkValidity()) {
-            formBtn.removeAttribute('disabled');
-        } else {
-            formBtn.setAttribute('disabled', '');
-        }
-    });
+   formInputs[i].addEventListener('input', function () {
+      // check form validation
+      if (form.checkValidity()) {
+         formBtn.removeAttribute('disabled');
+      } else {
+         formBtn.setAttribute('disabled', '');
+      }
+   });
 }
 
 // page navigation variables
@@ -87,16 +87,16 @@ const pages = document.querySelectorAll('[data-page]');
 
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
-    navigationLinks[i].addEventListener('click', function () {
-        for (let i = 0; i < pages.length; i++) {
-            if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-                pages[i].classList.add('active');
-                navigationLinks[i].classList.add('active');
-                window.scrollTo(0, 0);
-            } else {
-                pages[i].classList.remove('active');
-                navigationLinks[i].classList.remove('active');
-            }
-        }
-    });
+   navigationLinks[i].addEventListener('click', function () {
+      for (let i = 0; i < pages.length; i++) {
+         if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+            pages[i].classList.add('active');
+            navigationLinks[i].classList.add('active');
+            window.scrollTo(0, 0);
+         } else {
+            pages[i].classList.remove('active');
+            navigationLinks[i].classList.remove('active');
+         }
+      }
+   });
 }
